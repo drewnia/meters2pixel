49e407e7ae138795cba85e0b0b3009dbd4d21cc5
drewnia <sukharudze@gmail.com>
2023-11-11 20:44:09 +0300

    После подбора данных для расчета погрешности измерения профиля

--- meter2pixel.py
+++ meter2pixel.py
@@ -5,19 +5,19 @@ from math import *
 RIGEL = False
 h = 416  # FRAME height 4mp 1440, 2mp 1080, YOLO frame 416
 w = 416  # FRAME width
-dist = 50 # distance in meters, for which we would like to find pixels
+dist = 1 # distance in meters, for which we would like to find pixels
 
-alpha = radians(30)  # lens angle
-d = 25  # height of tower
+alpha = radians(50)  # lens angle
+d = 25  # height of a tower
 beta = radians(26)  # angle of position
 
 l2 = 110 # maximal distance
 
 if RIGEL:
-    alpha = radians(78.9)
-    d = 10
-    beta = radians(43)
-    l2 = 80
+    alpha = radians(120)
+    d = 10 # height of a rigel
+    beta = radians(45)
+    l2 = 70
 l1 = l2 - dist
 x1 = h * (sin(0.5 * alpha) + cos(0.5 * alpha) * tan(atan(l1 / d) + beta - radians(90))) / (sqrt(2 * (1 - cos(alpha))))
 x2 = h * (sin(0.5 * alpha) + cos(0.5 * alpha) * tan(atan(l2 / d) + beta - radians(90))) / (sqrt(2 * (1 - cos(alpha))))
